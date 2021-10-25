882fde88f2a3536f9372da2b2a5f049c87fee2cd
SampathE <elavarasan.sampath@agilysys.com>
2021-10-26 00:50:25 +0530

    waterflow simulation functionality with CSS changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { ConfigGridCreationComponent } from '../app/config-grid-creation/config-
 import { SimulateWaterflowComponent } from '../app/simulate-waterflow/simulate-waterflow.component';
 
 const routes: Routes = [
-  { path : '', component: ConfigGridCreationComponent },
-  { path : 'simulation', component: SimulateWaterflowComponent }
+  { path : 'config', component: ConfigGridCreationComponent },
+  { path: 'simulation', component: SimulateWaterflowComponent },
+  { path: '', redirectTo: '/config', pathMatch: 'full'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/simulate-waterflow/simulate-waterflow.component.ts
+++ src/app/simulate-waterflow/simulate-waterflow.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-simulate-waterflow',
@@ -8,11 +8,107 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 })
 export class SimulateWaterflowComponent implements OnInit {
 
+  gridCells: any = [];
+  flowStartRow: any = [];
+  gridBlocks: any = [];
+  gridOptions: any;
+  showSimulationStart: boolean = false;
+
   constructor(private route: ActivatedRoute, private router: Router) {
-    console.log(this.router.getCurrentNavigation().extras.state);
-  }
-  options: any;
-  ngOnInit(): void {
+    this.gridOptions = this.router.getCurrentNavigation().extras.state || {
+      noOfRows : 5,
+      noOfColumns : 5,
+      noOfObstructions : 3
+    };
   }
 
+  ngOnInit(): void {
+    this.gridCells = [];
+    for (let row = 0; row < this.gridOptions.noOfRows+1; row++) {
+      let rows = [];
+      for (let col = 0; col < this.gridOptions.noOfColumns; col++) {
+        rows.push({
+          rowIndex: row,
+          colIndex: col
+        });
+      }
+      this.gridCells.push(rows);
+    }
+    for (let block = 0; block < this.gridOptions.noOfObstructions; block++) {
+      this.gridBlocks.push({ id: block });
+    }
+  }
+  drop = (event) => {
+    let blockCell = event.dataTransfer.getData("draggedBlock");
+    this.gridBlocks[blockCell].moved = true;
+    const rowIndex = event.srcElement.attributes.rowindex.nodeValue;
+    const columnIndex = event.srcElement.attributes.colindex.nodeValue;
+    this.gridCells[rowIndex][columnIndex].blocked = true;
+    console.log(event);
+  }
+
+  allowDrop = (event) => {
+    event.preventDefault();
+  }
+
+  dragStart(event, blockCell) {
+    event.dataTransfer.setData("draggedBlock", blockCell.id);
+  }
+
+  startSimulation = () => {
+    this.showSimulationStart = true;
+  }
+
+  checkAllblocksMoved = () => {
+    return this.gridBlocks.filter(block => !block.moved).length > 0;
+  }
+
+  startFlowSimulation = (cell) => {
+    cell.flow = true;
+    this.flowDown(Object.assign({}, cell));
+  }
+
+  flowDown = (cell) => {
+    if (cell.rowIndex < this.gridCells.length && this.gridCells[cell.rowIndex + 1]) {
+      if (this.gridCells[cell.rowIndex + 1] && !this.gridCells[cell.rowIndex + 1][cell.colIndex].blocked) {
+        cell.rowIndex++;
+        this.gridCells[cell.rowIndex][cell.colIndex].flow = true;
+        if(this.gridCells[cell.rowIndex + 1])
+        this.flowDown(Object.assign({}, cell));
+      }
+      else {
+        setTimeout(() => { cell.colIndex + 1 < this.gridCells[cell.rowIndex].length ? this.flowRight(Object.assign({}, cell)): null });
+        setTimeout(() => { cell.colIndex -1 >= 0 ? this.flowLeft(Object.assign({}, cell)): null });
+      }
+    }
+  }
+
+  flowRight = (cell) => {
+    if (cell.colIndex < this.gridCells[cell.rowIndex].length && cell.rowIndex < this.gridCells.length) {
+      if (this.gridCells[cell.rowIndex][cell.colIndex + 1] && !this.gridCells[cell.rowIndex][cell.colIndex + 1].blocked) {
+        cell.colIndex++;
+        this.gridCells[cell.rowIndex][cell.colIndex].flow = true;
+      }
+      this.gridCells[cell.rowIndex + 1]
+      this.flowDown(Object.assign({}, cell));
+    }
+  }
+
+  flowLeft = (cell) => {
+    if (cell.colIndex >= 0 && cell.rowIndex < this.gridCells.length) {
+      if (this.gridCells[cell.rowIndex][cell.colIndex - 1] && !this.gridCells[cell.rowIndex][cell.colIndex - 1].blocked) {
+        cell.colIndex--;
+        this.gridCells[cell.rowIndex][cell.colIndex].flow = true;
+      }
+      this.gridCells[cell.rowIndex + 1]
+      this.flowDown(Object.assign({}, cell));
+    }
+  }
+
+  resetBlocks = () => {
+    const currentUrl = this.router.url;
+    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+        this.router.navigate([currentUrl], { state: { ...this.gridOptions } });
+    });
+  }
 }
